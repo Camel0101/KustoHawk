dcee8bf52a9ce92b757f08a8bd27427205e39d73
Bert-Jan <91258706+Bert-JanP@users.noreply.github.com>
2025-12-09 10:31:20 +0100

    Behavior anomaly events

--- Scripts/UIQueryToJSONFormat.ps1
+++ Scripts/UIQueryToJSONFormat.ps1
@@ -1,6 +1,9 @@
 $Query = "let Upn = '{UserPrincipalName}';
 let TimeFrame = {TimeFrame};
-Anomalies
-| where Entities has Upn or UserPrincipalName =~ Upn'"
+BehaviorEntities
+| where TimeGenerated >ago(TimeFrame)
+| where AccountUpn =~ Upn
+| join kind=inner (BehaviorInfo) on BehaviorId
+| project-reorder TimeGenerated, AccountUpn, Description, AttackTechniques"
 $Output = $Query -replace '\r','\r' -replace '\n','\n'
 Write-Output $Output
